51e31d666b1aa1d956eb8a91ad3bc0961a505cf1
Ramonprog <programador.ramon@gmail.com>
2023-10-23 08:10:45 -0300

    fix: corrigindo preço do produio

--- src/app/(home)/components/Categories.tsx
+++ src/app/(home)/components/Categories.tsx
@@ -6,7 +6,7 @@ const Categories = async () => {
     const categories = await prismaClient.category.findMany()
 
     return (
-        <div className="grid grid-cols-2 gap-y-2 gap-x-16">
+        <div className="grid grid-cols-2 gap-y-2 gap-x-16 md:grid-cols-3">
             {categories.map(item => (
                 <CategoryItem key={item.id} category={item} />
             ))}
